431a8157b2c89bfe3de359acc73fd79215604062
黄远林 <1481884060@qq.com>
2019-05-15 22:46:19 +0800

    添加替换

---
+++ PlasModel/PriceModle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasModel
+{
+    /// <summary>
+    /// 价格趋势列表
+    /// </summary>
+   public class PriceModle
+    {
+        public string PriDate { get; set; }
+        public string SmallClass { get; set; }
+        public string ManuFacturer { get; set; }
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Diff { get; set; }
+    }
+}
--- PlasWebApi/Controllers/HomeTempController.cs
+++ PlasWebApi/Controllers/HomeTempController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PlasModel;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -13,8 +15,13 @@ namespace PlasWebApi.Controllers
         /// 加载行情走势接口
         /// </summary>
         /// <returns></returns>
-        public List<string> GetHangQing()
+        public List<PriceModle> GetQuotation(string SmallClass,string Manufacturer,string Model,int pageindex=1,int pagesize=4)
         {
+            //var bll = new PlasBll.ProductBll();
+            //var list = new List<PriceModle>();
+            //var ds = new DataSet();
+            //ds = bll.getPriceFile(SmallClass, Manufacturer, Model, pageindex, pagesize);
+            //list = PlasCommon.ToolClass<PriceModle>.ConvertDataTableToModel(ds.Tables[0]);
             return null;
         }
 
