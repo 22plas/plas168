2c882a23d26a3641e1f0024bb6b599bb89c5c4c0
黄远林 <1481884060@qq.com>
2019-05-31 13:57:03 +0800

    数据缓存存储

---
+++ PlasModel/wordModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlasModel
+{
+    /// <summary>
+    /// 检索关键词
+    /// </summary>
+    public class wordModel
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 提取关键词
+        /// </summary>
+        public string Word { get; set; }
+    }
+}
--- PlasQueryWeb/Controllers/HomeController.cs
+++ PlasQueryWeb/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using PlasModel;
 using PlasModel.App_Start;
 using System;
+using System.Web.Caching;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PlasCommon;
 
 namespace PlasQueryWeb.Controllers
 {
@@ -60,33 +62,60 @@ namespace PlasQueryWeb.Controllers
         /// 生成JS
         /// </summary>
         /// <returns></returns>
-        public ActionResult SetJs()
+        public JsonResult SetJs()
         {
-            string path = Server.MapPath("../Scripts/SearchKeywordsJs.js");
-
-            // Delete the file if it exists. 
-            if (System.IO.File.Exists(path))
+            string keyword = string.Empty;
+            string strJson = string.Empty;//Json数据
+            if (!string.IsNullOrEmpty(Request["keyword"]))
             {
-                System.IO.File.Delete(path);
-            }
-            var dt = new PlasCommon.Common().Getsys_Autokey();
-            // Create the file. 
-            StreamWriter sr = System.IO.File.CreateText(path);
-            if (dt.Rows.Count > 0)
-            {
-                sr.WriteLine("'data':[");
-                for (var i = 0; i < dt.Rows.Count; i++)
-                {
-                    sr.WriteLine("{'id':'"+ i + "','word':'"+dt.Rows[i]["Word"].ToString().Trim()+"','description':''}");
-                }
-                sr.WriteLine("]");
-            }
-            //// 这里是f1的内容 
-            //// …… 
+                keyword = Request["keyword"].ToString();
+                var list = FindSearchsWord();
 
-            sr.Close();
-            return View();
+                strJson = Newtonsoft.Json.JsonConvert.SerializeObject(list.Where(p => p.Word.Contains(keyword)).ToList());
+            }
+            //string path = Server.MapPath("../Scripts/SearchKeywordsJs.js");
+
+            //// Delete the file if it exists. 
+            //if (System.IO.File.Exists(path))
+            //{
+            //    System.IO.File.Delete(path);
+            //}
+            //var dt = new PlasCommon.Common().Getsys_Autokey();
+            //// Create the file. 
+            //StreamWriter sr = System.IO.File.CreateText(path);
+            //if (dt.Rows.Count > 0)
+            //{
+            //    sr.WriteLine("'data':[");
+            //    for (var i = 0; i < dt.Rows.Count; i++)
+            //    {
+            //        sr.WriteLine("{'id':'"+ i + "','word':'"+dt.Rows[i]["Word"].ToString().Trim()+"','description':''}");
+            //    }
+            //    sr.WriteLine("]");
+            //}
+            ////// 这里是f1的内容 
+            ////// …… 
+
+            //sr.Close();
+            return Json(new { data = strJson }, JsonRequestBehavior.AllowGet);
         }
+
+
+        public IEnumerable<wordModel> FindSearchsWord()
+        {
+            var cache = CacheHelper.GetCache("commonData_Search");//先读取
+            if (cache == null)//如果没有该缓存
+            {
+                var dt = new PlasCommon.Common().Getsys_Autokey();
+                var enumerable = ToolClass<wordModel>.ConvertDataTableToModel(dt);
+                CacheHelper.SetCache("commonData_Search", enumerable);//添加缓存
+                return enumerable;
+            }
+            var result = (List<wordModel>)cache;//有就直接返回该缓存
+            return result;
+
+        }
+
+
 
 
     }
