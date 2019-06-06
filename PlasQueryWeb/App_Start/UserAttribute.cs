f9c91fbfc59ebf5228b347e535e1bb30c844e62f
黄远林 <1481884060@qq.com>
2019-06-06 17:28:12 +0800

    添加

---
+++ PlasQueryWeb/App_Start/UserAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlasQueryWeb.App_Start
+{
+    public class UserAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Cookies["AccountData"] == null)
+            {
+                filterContext.HttpContext.Response.Redirect("/MemberCenter/Login");
+            }
+        }
+    }
+}
